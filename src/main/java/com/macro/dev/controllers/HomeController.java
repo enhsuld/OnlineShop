b4b1483b5e402f1eb1e45e56cff330d689a691cc
enhsuld <z.enhsuld@yahoo.com>
2017-07-12 02:38:51 +0800

    s

--- src/main/java/com/macro/dev/controllers/HomeController.java
+++ src/main/java/com/macro/dev/controllers/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping(value="/")
+/*    @GetMapping(value="/")
     public String index(){
         return "index";
     }
@@ -19,7 +19,7 @@ public class HomeController {
     @GetMapping(value="/login")
     public String login(){
         return "login";
-    }
+    }*/
 
     @GetMapping(value="post/{id}")
     public String singlePost(){
