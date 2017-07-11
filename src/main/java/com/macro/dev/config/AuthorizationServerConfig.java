52bedb5ccc87fbea3d77b6651bf16e8b4ab0b06c
enhsuld <z.enhsuld@yahoo.com>
2017-07-12 03:08:41 +0800

    s

--- src/main/java/com/macro/dev/config/AuthorizationServerConfig.java
+++ src/main/java/com/macro/dev/config/AuthorizationServerConfig.java
@@ -1,17 +1,33 @@
 package com.macro.dev.config;
 
+import java.util.Arrays;
+
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 /**
  * Configures the authorization server.
@@ -19,55 +35,114 @@ import org.springframework.security.oauth2.provider.token.store.InMemoryTokenSto
  * together with any @Beans that implement AuthorizationServerConfigurer (there is a handy adapter implementation with empty methods).
  */
 @Configuration
+@PropertySource({ "classpath:persistence.properties" })
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
+    private Environment env;
+
+    @Autowired
+    @Qualifier("authenticationManagerBean")
     private AuthenticationManager authenticationManager;
 
-    /**
-     * Setting up the endpointsconfigurer authentication manager.
-     * The AuthorizationServerEndpointsConfigurer defines the authorization and token endpoints and the token services.
-     * @param endpoints
-     * @throws Exception
-     */
+    @Value("classpath:schema.sql")
+    private Resource schemaScript;
+
+    @Value("classpath:data.sql")
+    private Resource dataScript;
+
+
     @Override
-    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
-        endpoints.tokenStore(getTokenStore());
+    public void configure(final AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+        oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients)
+            throws Exception {
+            clients
+           .jdbc(dataSource())
+
+               /* .inMemory().withClient("sampleClientId").authorizedGrantTypes("implicit")
+                .scopes("read", "write", "foo", "bar").autoApprove(false).accessTokenValiditySeconds(3600)
+
+                .and().withClient("fooClientIdPassword").secret("secret")
+                .authorizedGrantTypes("password", "authorization_code", "refresh_token").scopes("foo", "read", "write")
+                .accessTokenValiditySeconds(3600) // 1 hour
+                .refreshTokenValiditySeconds(2592000) // 30 days
+
+                .and().withClient("barClientIdPassword").secret("secret")
+                .authorizedGrantTypes("password", "authorization_code", "refresh_token").scopes("bar", "read", "write")
+                .accessTokenValiditySeconds(3600) // 1 hour
+                .refreshTokenValiditySeconds(2592000) // 30 days*/
+
+
+              /*  .withClient("sampleClientId")
+                .authorizedGrantTypes("implicit")
+                .scopes("read")
+                .autoApprove(true)
+                .and()
+                .withClient("clientIdPassword")
+                .secret("secret")
+                .authorizedGrantTypes(
+                        "password","authorization_code", "refresh_token")
+                .scopes("read")*/;
+    }
+
+    @Override
+    public void configure(final AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        // @formatter:off
+        final TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+        tokenEnhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer()));
+        endpoints.tokenStore(tokenStore())
+                // .accessTokenConverter(accessTokenConverter())
+                .tokenEnhancer(tokenEnhancerChain).authenticationManager(authenticationManager);
+        // @formatter:on
+    }
+
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        defaultTokenServices.setSupportRefreshToken(true);
+        return defaultTokenServices;
     }
 
     @Bean
-    public TokenStore getTokenStore(){
-        return new InMemoryTokenStore();
+    public TokenEnhancer tokenEnhancer() {
+        return new CustomTokenEnhancer();
     }
 
-    /**
-     * Setting up the clients with a clientId, a clientSecret, a scope, the grant types and the authorities.
-     * @param clients
-     * @throws Exception
-     */
-    @Override
-    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory().
-                withClient("my-trusted-client")
-                .authorizedGrantTypes("client_credentials", "password")
-                .authorities("ROLE_CLIENT","ROLE_TRUSTED_CLIENT").scopes("read","write","trust")
-                .resourceIds("oauth2-resource").accessTokenValiditySeconds(5000).secret("secret");
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(final DataSource dataSource) {
+        final DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(databasePopulator());
+        return initializer;
     }
 
-    /**
-     * We here defines the security constraints on the token endpoint.
-     * We set it up to isAuthenticated, which returns true if the user is not anonymous
-     * @param security the AuthorizationServerSecurityConfigurer.
-     * @throws Exception
-     */
-    @Override
-    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        security.checkTokenAccess("isAuthenticated()");
+    private DatabasePopulator databasePopulator() {
+        final ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(schemaScript);
+        populator.addScript(dataScript);
+        return populator;
     }
 
+    @Bean
+    public DataSource dataSource() {
+        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+        dataSource.setUrl(env.getProperty("jdbc.url"));
+        dataSource.setUsername(env.getProperty("jdbc.user"));
+        dataSource.setPassword(env.getProperty("jdbc.pass"));
+        return dataSource;
+    }
 
-
-
+    @Bean
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource());
+    }
 }
